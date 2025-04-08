ac6122e4a320823bc7e96a01ffb6e47d1fa8d719
kchavinson <kc.chavinson@menloschool.org>
2025-04-08 13:17:42 -0700

    Completed DFS BFS and get Solution methods

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -30,10 +30,27 @@ public class MazeSolver {
      * @return An arraylist of MazeCells to visit in order
      */
     public ArrayList<MazeCell> getSolution() {
+        ArrayList<MazeCell> solutionPath = new ArrayList<>();
+        Stack<MazeCell> mazeCells = new Stack<>();
+        MazeCell currentCell = maze.getEndCell();
 
-        // get each parent and add to an arraylist then return it
-        return null;
-
+        // Solve going backwards starting from end cell
+        while (currentCell != null)
+        {
+            mazeCells.push(currentCell);
+            // If you are the start, stop
+            if (currentCell == maze.getStartCell())
+            {
+                break;
+            }
+            currentCell = currentCell.getParent();
+        }
+        // Stack is Lifo so add all of it into an arraylist to get correct path
+        while (!mazeCells.isEmpty())
+        {
+            solutionPath.add(mazeCells.pop());
+        }
+        return solutionPath;
     }
 
     /**
@@ -43,8 +60,33 @@ public class MazeSolver {
     public ArrayList<MazeCell> solveMazeDFS() {
         // TODO: Use DFS to solve the maze
         // Explore the cells in the order: NORTH, EAST, SOUTH, WEST
-        return null;
+        MazeCell currentCell = maze.getStartCell();
+        Stack<MazeCell> mazeCells = new Stack<MazeCell>();
 
+        while (currentCell != maze.getEndCell())
+        {
+            // Gets the current row and col
+            int row = currentCell.getRow();
+            int col = currentCell.getCol();
+
+            // In the directional order, it sees if it can add the cells to the stack and adds last one
+            checkStackCell(row -1, col, currentCell, mazeCells);
+            checkStackCell(row, col +1, currentCell, mazeCells);
+            checkStackCell(row + 1, col, currentCell, mazeCells);
+            checkStackCell(row, col -1, currentCell, mazeCells);
+            currentCell = mazeCells.pop();
+        }
+        // Returns solution
+        return getSolution();
+    }
+    public void checkStackCell(int row, int col, MazeCell currentCell, Stack mazeCells){
+        // Makes sure the cell hasn't been explored and is valid, then adds it to stack if it is and explores it aswell
+        if (maze.isValidCell(row, col))
+        {
+            mazeCells.push(maze.getCell(row , col));
+            maze.getCell(row, col).setExplored(true);
+            maze.getCell(row, col).setParent(currentCell);
+        }
     }
 
     /**
@@ -54,8 +96,34 @@ public class MazeSolver {
     public ArrayList<MazeCell> solveMazeBFS() {
         // TODO: Use BFS to solve the maze
         // Explore the cells in the order: NORTH, EAST, SOUTH, WEST
-        return null;
+        MazeCell currrentCell = maze.getStartCell();
+        Queue<MazeCell> mazeCells = new LinkedList<MazeCell>();
 
+        while (currrentCell != maze.getEndCell())
+        {
+            // Gets row and column of the current cell
+            int row = currrentCell.getRow();
+            int col = currrentCell.getCol();
+
+            // In the directional order, it sees if it can add the cells to the queue
+            checkQueueCell(row -1, col, currrentCell, mazeCells);
+            checkQueueCell(row, col +1, currrentCell, mazeCells);
+            checkQueueCell(row +1, col, currrentCell, mazeCells);
+            checkQueueCell(row, col -1, currrentCell, mazeCells);
+            currrentCell = mazeCells.poll();
+        }
+        // Returns solution
+        return getSolution();
+    }
+
+    public void checkQueueCell(int row, int col, MazeCell currentCells, Queue mazeCells){
+        // Makes sure the cell hasn't been explored and is valid, then adds it to queue if it is and explores it aswell
+        if (maze.isValidCell(row, col))
+        {
+            mazeCells.add(maze.getCell(row , col));
+            maze.getCell(row, col).setExplored(true);
+            maze.getCell(row, col).setParent(currentCells);
+        }
     }
 
     public static void main(String[] args) {
